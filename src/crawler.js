84c637befaf03584bf95faa90519d709d1aa0bb8
tadahiroueta <tadahiroueta@gmail.com>
2023-02-17 22:25:39 -0600

    Now using estimated cost

--- src/crawler.js
+++ src/crawler.js
@@ -4,24 +4,22 @@ const cookies = require('../data/cookies.json');
 
 
 const EXPLORE_URL = "https://www.collegevine.com/schools/hub/all"
-const SIGNS = ["$", ",", "%", "\n"]
+const SIGNS = ["$", ",", "%", "\n", "/ year *"]
 const WAIT_OPTIONS = { waitUntil: 'networkidle2', timeout: 0 }
 const SELECTORS = { 
     SEARCH: "#react-select-2-input", 
-    IN_STATE_COST: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--financials-cost-before-aid > div.card-body > div:nth-child(1) > div.col-auto > strong",
-    OUT_STATE_COST: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--financials-cost-before-aid > div.card-body > div:nth-child(3) > div.col-auto > strong",
 
     PAGE_SELECTORS: {
-        size: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--overview > div > strong:nth-child(5)",
+        size: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--overview > div > strong:nth-child(3)",
         setting: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-4 > div.card.t--location > div > div.row.align-items-end > div:nth-child(3) > strong",
         region: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-4 > div.card.t--location > div > div.row.align-items-center > div.col-auto > strong",
         yield: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--admission-stats > div.card-body > div > div:nth-child(2) > div.d-flex.align-items-baseline.mb-4 > div.large.font-weight-bold.fw-bold.mr-1",
         collegeVineChances: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--chancing > div > div.row.align-items-center.mb-3 > div:nth-child(1) > div > div.col-6.order-1.order-lg-2.my-auto > h3",
         acceptance: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--chancing > div > div.row.align-items-center.mb-3 > div.col-12.col-lg-6.py-3.my-auto > h3",
         state: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-4 > div.card.t--location > div > div.row.mb-4 > div.col.pl-0 > div:nth-child(2)",
-        difficulty: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--chancing > div > h2"
-    }
-}
+        difficulty: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--chancing > div > h2",
+        cost: "div > div > div > div:nth-child(1) > div > div > div > div.mx-auto > div.flex-grow-1.min-height-0.overflow-y-auto.overflow-x-hidden.px-3.pt-4.bg-very-light-grey > div > div.col-12.col-md-8 > div.card.t--financials-personal-estimate > div > div.large.font-weight-bold.fw-bold"
+}}
 
 /**
  * Gets puppeteer browser
@@ -68,7 +66,7 @@ const getCollegeData = async (name, browser) => {
                         const number = parseInt(string)
                         if (number <= 100) string = (number / 100).toString()
                     }
-                    return string
+                    return string.trim()
                 }
 
                 const data = { ...SELECTORS.PAGE_SELECTORS }
@@ -82,7 +80,7 @@ const getCollegeData = async (name, browser) => {
                 }
 
                 data.state = data.state.split(" ").find(word => word.length === 2 && word == word.toUpperCase()).substring(0, 2) // looks for a two letter word that is all caps
-                data.cost = clean(document.querySelector(data.state === "TX" ? SELECTORS.IN_STATE_COST : SELECTORS.OUT_STATE_COST).innerText) // gets cost based on state
+                // data.cost = clean(document.querySelector(data.state === "TX" ? SELECTORS.IN_STATE_COST : SELECTORS.OUT_STATE_COST).innerText) // gets cost based on state
 
                 if (data.difficulty) {
                     const split = data.difficulty.split(" ")
